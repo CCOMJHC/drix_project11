ceb928330935e7b22a51ebb0804b74ad72115401
Roland Arsenault <roland@ccom.unh.edu>
2021-07-23 10:12:49 -0400

    Fix how env vars are applied

--- scripts/start_tmux_robobox_interface.sh
+++ scripts/start_tmux_robobox_interface.sh
@@ -4,10 +4,9 @@ DAY=$(date "+%Y-%m-%d")
 LOGDIR="/home/field/project11/log/${DAY}"
 mkdir -p "$LOGDIR"
 
-export ROS_MASTER_URI=http://192.168.0.201:11311
-export ROS_IP=$ROBOBOX_ROS_IP 
-
 tmux new -d -s robobox
+tmux send-keys "export ROS_MASTER_URI=http://192.168.0.201:11311" C-m
+tmux send-keys "export ROS_IP=$ROBOBOX_ROS_IP" C-m
 tmux send-keys "rosrun rosmon rosmon --name=rosmon_p11_robobox drix_project11 robobox_interface.launch logDirectory:=${LOGDIR}" C-m
 
 tmux a -t robobox
