300b1bd7e9cefc3eb9124f6759086cecdb4f6784
Roland Arsenault <roland@ccom.unh.edu>
2021-07-23 09:46:00 -0400

    Fix typo

--- scripts/start_tmux_robobox_interface.sh
+++ scripts/start_tmux_robobox_interface.sh
@@ -8,6 +8,6 @@ export ROS_MASTER_URI=http://192.168.0.201:11311
 export ROS_IP=$ROBOBOX_ROS_IP 
 
 tmux new -d -s robobox
-tmux send-keys "rosrun rosmon rosmon --name=rosmon_p11_robobox drix_project11 robobox_interface logDirectory:=${LOGDIR}" C-m
+tmux send-keys "rosrun rosmon rosmon --name=rosmon_p11_robobox drix_project11 robobox_interface.launch logDirectory:=${LOGDIR}" C-m
 
 tmux a -t robobox
