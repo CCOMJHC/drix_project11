17c479b7af2cc8c7c5d41a82da8cc92409960dd6
Roland Arsenault <roland@ccom.unh.edu>
2021-07-23 09:44:45 -0400

    Add script and launch file for robobox interface

---
+++ scripts/start_tmux_robobox_interface.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+DAY=$(date "+%Y-%m-%d")
+LOGDIR="/home/field/project11/log/${DAY}"
+mkdir -p "$LOGDIR"
+
+export ROS_MASTER_URI=http://192.168.0.201:11311
+export ROS_IP=$ROBOBOX_ROS_IP 
+
+tmux new -d -s robobox
+tmux send-keys "rosrun rosmon rosmon --name=rosmon_p11_robobox drix_project11 robobox_interface logDirectory:=${LOGDIR}" C-m
+
+tmux a -t robobox
