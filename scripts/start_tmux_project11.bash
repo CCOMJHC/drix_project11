2cf003587017875bf978793f5555fc2f0122a1d7
Field User <field@ccom.unh.edu>
2021-11-05 14:25:30 +0000

    Split km_convert out of start script

--- scripts/start_tmux_project11.bash
+++ scripts/start_tmux_project11.bash
@@ -12,20 +12,13 @@ while ! ping -c 1 -W 1 mdt; do
 	sleep 1
 done
 
-sleep 10
+sleep 5
 
 source /opt/ros/noetic/setup.bash
 source /home/field/project11/catkin_ws/devel/setup.bash
 
 export ROS_MASTER_URI=http://mdt:11311
 export ROS_IP=192.168.8.180
-
-#open a new tmux session and launch the km_convert script
-#this is used to split the km_binary from the phins 
-
-/usr/bin/tmux new -d -s km_convert
-/usr/bin/tmux send-keys -t km_convert "cd ~/km_convert/" C-m
-/usr/bin/tmux send-keys -t km_convert "./km_convert.py" C-m
 
 #open a new tmux session, source the ros master, and run the drix.launch file to startup the backset project 11
 
