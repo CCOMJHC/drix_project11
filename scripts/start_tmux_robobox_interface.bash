79341ee6926330b4ddecb0aafe3d4c828d5f2e66
Roland Arsenault <roland@ccom.unh.edu>
2022-07-25 10:06:57 -0400

    Update start script

--- scripts/start_tmux_robobox_interface.bash
+++ scripts/start_tmux_robobox_interface.bash
@@ -30,7 +30,7 @@ export ROS_IP=$ROBOBOX_ROS_IP
 /usr/bin/tmux new -d -s robobox
 /usr/bin/tmux send-keys -t robobox "source /home/field/.bashrc" C-m
 /usr/bin/tmux send-keys -t robobox "source /home/field/project11/catkin_ws/src/drix_project11/scripts/robobox_as_core.bash" C-m
-/usr/bin/tmux send-keys -t robobox "rosrun rosmon rosmon --name=rosmon_p11_robobox drix_project11 robobox_interface.launch logDirectory:=${LOGDIR}" C-m
+/usr/bin/tmux send-keys -t robobox "rosrun rosmon_core rosmon --name=rosmon_p11_robobox drix_project11 robobox_interface.launch logDirectory:=${LOGDIR}" C-m
 
 #tmux a -t robobox
 
