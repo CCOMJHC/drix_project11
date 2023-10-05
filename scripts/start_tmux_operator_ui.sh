556df29f38f3081670688b1ee3c73315511b8200
Field User <field@ccom.unh.edu>
2023-10-05 03:56:16 +0000

    Update nav params

--- scripts/start_tmux_operator_ui.sh
+++ scripts/start_tmux_operator_ui.sh
@@ -9,5 +9,5 @@ source /home/field/.bashrc
 tmux new -d -s project11_ui
 tmux send-keys -t project11_ui "source /home/field/.bashrc" C-m
 tmux send-keys -t project11_ui "source /home/field/project11/catkin_ws/src/drix_project11/scripts/robobox_as_core.bash" C-m
-tmux send-keys -t project11_ui "rosrun rosmon rosmon --name=rosmon_p11_operator_ui drix_project11 operator_ui.launch dual_camp:=true drixNumber:=8" C-m
+tmux send-keys -t project11_ui "rosrun rosmon rosmon --name=rosmon_p11_operator_ui drix_project11 operator_ui.launch drixNumber:=8 backgroundChart:=/home/field/data/noaa_custom_charts/Hawaii.pdf" C-m
 tmux a -t project11_ui
